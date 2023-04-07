7e89e8743874f9efd0628dfe8187e4312261b792
Alexander Gröttrup <groettrup@fu-berlin.de>
2023-04-07 15:40:27 +0200

    Add logic to turn image into graytscale and create the output

--- main.js
+++ main.js
@@ -1,13 +1,49 @@
 url = undefined
 
+function tograyscale(ctx, canvas) {
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+    let outstring = "{ "
+    let x = 0
+    for (let i = 0; i < data.length; i += 4) {
+        const avg = ((data[i] + data[i + 1] + data[i + 2]) / 3)>>4;
+        if((i>>2)%2==0){
+            x = avg << 4
+        } else {
+            x += avg
+            if(i!=4)outstring += ", "
+            outstring += "0x" + x.toString(16);
+        }
+        data[i] = avg<<4; // red
+        data[i + 1] = avg<<4; // green
+        data[i + 2] = avg<<4; // blue
+    }
+    // last 4 bits are not printed in the loop if the last i is divisible by 8
+    // without a reminder. with `length == last_i + 4` i can test whether the lenght
+    // is not divisible by 8
+    if((data.length>>2)%2==1){
+        outstring += ", 0x" + x.toString(16);
+    }
+    outstring += " } "
+    const out = document.getElementById("out")
+    out.onclick = () => {
+        out.onclick = null
+        navigator.clipboard.writeText(outstring)
+    }
+    ctx.putImageData(imageData, 0, 0);
+};
+
 function onSizeChange(change_event){
     const canvas = document.getElementById("imgcanvas")
+    let val = change_event.target.value
     switch(change_event.target.id){
         case 'winput':
-            canvas.width = change_event.target.value
+            if(val <= 960) canvas.width = val
+            else change_event.target.value = canvas.width
             break;
         case 'hinput':
-            canvas.height = change_event.target.value
+            if(val <= 540) canvas.height = val
+            else change_event.target.value = canvas.height
             break;
     }
     console.log(`${change_event.target.id}: ${canvas.width} / ${canvas.height}`);
@@ -15,7 +51,9 @@ function onSizeChange(change_event){
     if(url){
         const img = new Image()
         img.onload = () => {
-            canvas.getContext("2d").drawImage(img,0,0);
+            let ctx = canvas.getContext("2d")
+            ctx.drawImage(img,0,0);
+            tograyscale(ctx,canvas)
         }
         img.src = url
     }   
@@ -29,8 +67,10 @@ function onImageUpload(change_event){
     url  = URL.createObjectURL(blob)
     const img = new Image()
     img.onload = () => {
-        let ctx = document.getElementById("imgcanvas").getContext("2d");
+        const canvas = document.getElementById("imgcanvas")
+        const ctx = canvas.getContext("2d");
         ctx.drawImage(img,0,0);
+        tograyscale(ctx, canvas);
     }
     img.src = url;
 }
